39924ec060a1838f3c309c0c34d684bab5cb65f4
Felipe322 <37180544@uaz.edu.mx>
2022-10-04 22:39:48 -0500

    Agrega PDF completo de ficha

--- correspondencia/ficha/views.py
+++ correspondencia/ficha/views.py
@@ -1,7 +1,11 @@
 import io
-
+import os.path
 from django.http import FileResponse
 from reportlab.pdfgen import canvas
+
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Image
+from reportlab.lib.units import cm
+from reportlab.lib import colors
 
 from django.conf import settings
 from django.shortcuts import render, redirect, get_object_or_404
@@ -85,18 +89,55 @@ def fichaPDF(request, pk):
     ficha = get_object_or_404(Ficha, id_ficha=pk)
     
     # Create the PDF object, using the buffer as its "file."
-    p = canvas.Canvas(buffer)
+    pdf = canvas.Canvas(buffer)
 
-    # imagen = settings.MEDIA_ROOT+'logo-cozcyt.png'
-    # p.drawImage(imagen, 40, 750, 120, 90, preserveAspectRatio=True)
+    #Establecemos el tamaño de letra en 13 y el tipo de letra Helvetica
+    pdf.setFont("Helvetica", 13)
+    #Dibujamos una cadena en la ubicación X,Y especificada
+    pdf.drawString(100, 720, u"FICHA DE CONTROL Y SEGUIMIENTO DE CORRESPONDENCIA")
 
-    # Draw things on the PDF. Here's where the PDF generation happens.
-    # See the ReportLab documentation for the full list of functionality.
-    p.drawString(100, 100, "Hello world." + ficha.nombre_firma)
+    # Agrega imagen a PDF
+    imagen = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../static/images/logo-cozcyt.png')
+    pdf.drawImage(imagen, 50, 725, width=170, preserveAspectRatio=True, mask='auto')
+
+    #Creamos una tupla de encabezados para neustra tabla
+    encabezados = ('                                   ', 'No. de ficha: ' + str(ficha.id_ficha))
+
+    #Creamos una lista de tuplas que van a contener la tabla
+    fecha = (['Fecha: \n', str(ficha.fecha) + '\n'])
+    num_doc = ('Numero de Documento: \n', str(ficha.num_documento) + '\n')
+    fecha_doc = ('Fecha del Documento: \n', str(ficha.fecha_documento) + '\n')
+    dependencia = ('Dependencia Procedente:\n\n ', str(ficha.dependencia) + '\n\n')
+    firma = ('Nombre de quién firma:\n ', str(ficha.nombre_firma) + '\n')
+    asunto = ('Asunto:\n\n\n\n ', str(ficha.asunto) + '\n\n\n\n')
+    area = ('Area del COZCYT a la que se turna: \n\n\n', str(ficha.area_turnada)+ '\n\n\n')
+    instruccion = ('Instrucción: \n\n\n\n', str(ficha.instruccion) + '\n\n\n\n')
+    resolucion = ('Resolucion:\n\n\n\n ', str(ficha.resolucion) + '\n\n\n\n')
+    fecha_firma = ('Fecha y Firma de quién recibe:\n\n\n\n ', str(ficha.fecha_recibido)+ '\n\n\n\n')
+
+    detalles = [fecha] + [num_doc] + [fecha_doc] + [dependencia] + [firma] + [asunto] + [area] + [instruccion] + [resolucion] + [fecha_firma]
+
+    #Establecemos el tamaño de cada una de las columnas de la tabla
+    detalle_orden = Table([encabezados] + detalles, colWidths=[6.3 * cm, 10 * cm])
+    #Aplicamos estilos a las celdas de la tabla
+    detalle_orden.setStyle(TableStyle(
+        [
+            #La primera fila(encabezados) va a estar centrada
+            ('ALIGN',(0,0),(3,0),'CENTER'),
+            #Los bordes de todas las celdas serán de color negro y con un grosor de 1
+            ('GRID', (0, 0), (-1, -1), 1, colors.black), 
+            #El tamaño de las letras de cada una de las celdas será de 10
+            ('FONTSIZE', (0, 0), (-1, -1), 10)
+        ]
+    ))
+    # Establecemos el tamaño de la hoja que ocupará la tabla 
+    detalle_orden.wrapOn(pdf, 80, 500)
+    #Definimos la coordenada donde se dibujará la tabla
+    detalle_orden.drawOn(pdf, 69, 190)
 
     # Close the PDF object cleanly, and we're done.
-    p.showPage()
-    p.save()
+    pdf.showPage()
+    pdf.save()
 
     # FileResponse sets the Content-Disposition header so that browsers
     # present the option to save the file.
